6b59628c833c2e0b723b0e686fca54d3a31bccda
Konstantin <localhost@local.domain>
2020-07-07 20:41:26 +0300

    Add animation; Remove redundatn "br" tag; Add min css and js files; Some fixes;

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,7 @@ var contactOpen = document.querySelector(".contact__show");
 var contactPopup = document.querySelector(".modal-contact");
 var contactClose = document.querySelector(".modal-contact__close");
 var contactForm = document.querySelector(".contact-form");
-var contactLogin = contactPopup.querySelector(".name");
+var contactName = contactPopup.querySelector(".name");
 var contactEmail = contactPopup.querySelector(".email");
 
 var mapOpen = document.querySelector(".map__show");
@@ -10,19 +10,48 @@ var mapPopup = document.querySelector(".modal-map");
 var mapClose = document.querySelector(".modal-map__close");
 
 
+var isStorageSupport = true;
+var storageName = "";
+var storageEmail = "";
+
+try {
+  storageName = localStorage.getItem("name");
+  storageEmail = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
+
 contactOpen.addEventListener("click", function(evt) {
   evt.preventDefault();
   contactPopup.classList.add("modal-show");
-  contactLogin.focus();
+  if(storageName) {
+    contactName.value = storageName;
+    contactEmail.focus();
+  } else if (storageEmail) {
+    contactEmail.value = storageEmail;
+    contactName.focus();
+  } else {
+    contactName.focus();
+  }
 });
 
 contactClose.addEventListener("click", function(evt) {
   evt.preventDefault();
   contactPopup.classList.remove("modal-show");
+  contactPopup.classList.remove("modal-error");
 });
 
 contactForm.addEventListener("submit", function(evt) {
-  evt.preventDefault();
+  if(!contactName.value || !contactEmail.value ) {
+    evt.preventDefault();
+    contactPopup.classList.remove("modal-error");
+    contactPopup.offsetWidth = contactPopup.offsetWidth;
+    contactPopup.classList.add("modal-error");
+    contactName.focus();
+  } else {
+    localStorage.setItem('name', contactName.value);
+    localStorage.setItem('email', contactEmail.value)
+  }
 });
 
 
@@ -36,3 +65,18 @@ mapClose.addEventListener("click", function(evt) {
   mapPopup.classList.remove("modal-show");
 })
 
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (contactPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      contactPopup.classList.remove("modal-show");
+    }
+  }
+
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
